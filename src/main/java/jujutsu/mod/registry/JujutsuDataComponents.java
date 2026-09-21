44bab7c7c082060cf5e7ec86e3cd8b6a6afe2be2
stravvbery <inpeisuruofiical901@gmail.com>
2026-09-21 11:33:33 +0300

    archive(incidents): remove cursed object data component

--- src/main/java/jujutsu/mod/registry/JujutsuDataComponents.java
+++ src/main/java/jujutsu/mod/registry/JujutsuDataComponents.java
@@ -3,16 +3,11 @@ package jujutsu.mod.registry;
 import jujutsu.mod.JujutsuMod;
 import jujutsu.mod.character.nobara.projectjjk.ProjectJjkResonanceRemnant;
 import jujutsu.mod.character.nobara.projectjjk.RemnantVisualType;
-import jujutsu.mod.cursedincident.object.CursedObjectState;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 
 public final class JujutsuDataComponents {
-	public static final DataComponentType<CursedObjectState> CURSED_OBJECT_STATE = DataComponentType
-			.<CursedObjectState>builder()
-			.persistent(CursedObjectState.CODEC)
-			.networkSynchronized(CursedObjectState.STREAM_CODEC)
 			.build();
 	public static final DataComponentType<ProjectJjkResonanceRemnant> RESONANCE_TARGET = DataComponentType
 			.<ProjectJjkResonanceRemnant>builder()
@@ -30,6 +25,5 @@ public final class JujutsuDataComponents {
 	public static void register() {
 		Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, JujutsuMod.id("resonance_target"), RESONANCE_TARGET);
 		Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, JujutsuMod.id("resonance_remnant_visual"), RESONANCE_REMNANT_VISUAL);
-		Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, JujutsuMod.id("cursed_object_state"), CURSED_OBJECT_STATE);
 	}
 }
